3220efc56dc61e71e787cb6984ee5c746a51ad50
nadianewaz <aninditaahmed@proton.me>
2023-11-27 23:56:17 +0600

    front-page update

--- src/app/component/banner/banner.js
+++ src/app/component/banner/banner.js
@@ -1,12 +1,16 @@
 import { XMarkIcon } from '@heroicons/react/20/solid'
 import '../banner/banner';
+import Classes from '@/app/classes/page';
 
 export default function Banner() {                   
   return (
     
     <>
     <div className="bg-no-repeat" style={{backgroundImage:`url('/img/banner.jpg')`, width: '100%',
-      height: '1000px'}}>
+      height: '600px'}}>
+        <h1 className='font-bold text-3xl pt-20 pl-12'>৬ষ্ঠ-এইচএসসি শ্রেণির
+অনলাইন ব্যাচে ভর্তি চলছে!</h1>
+  <Classes></Classes>
     </div>
     </>
     
